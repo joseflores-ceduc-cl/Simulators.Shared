5b967df266bb7f01c998c5ecd8896c46927f9fc0
Jose Leonardo Flores <encargado1.simulacionvr@ceduc.cl>
2026-06-12 11:52:25 -0400

    initial commit

---
+++ Runtime/Scripts/TestSharedUtils.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Ceduc.Simulators.Shared
+{
+    public static class TestSharedUtils
+    {
+        public static void LogMessage(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public static void LogWarning(string message) 
+        {
+            Debug.Log(message); 
+        }
+
+    }
+
+}
